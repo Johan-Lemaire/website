ab12d1b8b9b9eafe210a04ad89c0cd22abd77a2c
Johan-Lemaire <johanlemaire.pro@gmail.com>
2020-05-10 16:15:59 +0200

    add skills content on about page with little animations entrance

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -8,28 +8,45 @@ const About = () => {
 
   return (
     <div className={aboutStyles.about}>
-      <h1 className={aboutStyles.about_title}>
-        <span className="bounce">A&nbsp;</span>
-        <span className="bounce">p</span>
-        <span className="bounce">r</span>
-        <span className="bounce">o</span>
-        <span className="bounce">p</span>
-        <span className="bounce">o</span>
-        <span className="bounce">s</span>
-      </h1>
-      <p className={aboutStyles.about_text}>
-        Professionnellement connecté au monde du développement web depuis une année.
-      </p>
-      <p className={aboutStyles.about_text}>
-        Je suis une personne bien organisée dans son travail qui est sensible au rendu final et qui a le souci du détail.
-        Je suis passionné par l&apos;univers du gaming, de l&apos;environnement informatique, de formule 1 et plus récemment la science de l&apos;univers.
-      </p>
-      <p className={aboutStyles.about_text}>
-        Intéressé par tout ce qui touche la face visible d&apos;un projet, le front-end, et toujours
-        curieux d&apos;en apprendre plus chaque jour, j&apos;ai pour ambition de travailler sur de nouveaux projets avec d&apos;autres personnes
-        et apporter ma positivité et mon énergie sur le plan humain et professionel.
-      </p>
-      <ModalImage className={aboutStyles.about_timeline} small={timeline} large={timeline} alt="Chronologie de mon parcours professionnel" />
+      <div className={aboutStyles.about_left}>
+        <h1 className={aboutStyles.about_left_title}>
+          <span className="bounce">A&nbsp;</span>
+          <span className="bounce">p</span>
+          <span className="bounce">r</span>
+          <span className="bounce">o</span>
+          <span className="bounce">p</span>
+          <span className="bounce">o</span>
+          <span className="bounce">s</span>
+        </h1>
+        <p className={aboutStyles.about_left_text}>
+          Professionnellement connecté au monde du développement web depuis une année.
+        </p>
+        <p className={aboutStyles.about_left_text}>
+          Je suis une personne bien organisée dans son travail qui est sensible au rendu final et qui a le souci du détail.
+          Je suis passionné par l&apos;univers du gaming, de l&apos;environnement informatique, de formule 1 et plus récemment la science de l&apos;univers.
+        </p>
+        <p className={aboutStyles.about_left_text}>
+          Intéressé par tout ce qui touche la face visible d&apos;un projet, le front-end, et toujours
+          curieux d&apos;en apprendre plus chaque jour, j&apos;ai pour ambition de travailler sur de nouveaux projets avec d&apos;autres personnes
+          et apporter ma positivité et mon énergie sur le plan humain et professionel.
+        </p>
+        <ModalImage className={aboutStyles.about_left_timeline} small={timeline} large={timeline} alt="Chronologie de mon parcours professionnel" />
+      </div>
+      <div className={aboutStyles.about_right}>
+        <ul className={aboutStyles.about_right_list}>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_html}`}>HTML 5</li>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_css}`}>CSS 3</li>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_js}`}>JavaScript</li>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_jq}`}>jQuery</li>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_saas}`}>SASS</li>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_react}`}>React</li>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_redux}`}>Redux</li>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_gatsby}`}>Gatsby</li>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_npm}`}>Npm</li>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_yarn}`}>Yarn</li>
+          <li className={`${aboutStyles.about_right_list_item} ${aboutStyles.about_right_list_item_git}`}>Git</li>
+        </ul>
+      </div>
     </div>
   );
 };
